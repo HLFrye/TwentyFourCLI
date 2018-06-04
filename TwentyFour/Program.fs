cbb705db6f93a809abade4500a32a642994aad50
HLFrye <HLFrye@github>
2018-06-04 12:22:35 -0500

    Fixed typo

--- TwentyFour/Program.fs
+++ TwentyFour/Program.fs
@@ -73,7 +73,7 @@ let buildAllTrees (opers:list<expr * expr -> expr>) (constants:list<expr>) =
     let (constant1, constant2, constant3, constant4) = (constants.[0], constants.[1], constants.[2], constants.[3])
     let tree1 = oper3(oper1(constant1, constant2), oper2(constant3, constant4))
     let tree2 = oper3(oper2(oper1(constant1, constant2), constant3), constant4)
-    let tree3 = oper3(constant1, oper2(constant2, oper3(constant3, constant4)))
+    let tree3 = oper3(constant1, oper2(constant2, oper1(constant3, constant4)))
     let tree4 = oper3(oper2(constant1, oper1(constant2, constant3)), constant4)
     let tree5 = oper3(constant1, oper2(oper1(constant2, constant3), constant4))
 
